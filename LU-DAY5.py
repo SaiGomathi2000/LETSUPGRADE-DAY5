9775507b71d546291ce51d0cfe127fab97b9552b
SaiGomathi2000 <55175874+SaiGomathi2000@users.noreply.github.com>
2020-09-13 19:33:11 +0530

    Add files via upload
    ASSIGNMENT I OF DAY5

---
+++ LU-DAY5.py
@@ -0,0 +1,18 @@
+lst=[1,5,6,4,1,2,3,5]
+sublst2=[1,1,5]
+for i in lst:
+    if i==sublst2[0]:
+        while i>=0:
+            lst.pop(i)
+            i=i-1
+            lst.pop(0)
+for i in lst:
+        if i==sublst2[2]:
+            a="true"
+            break
+        else:
+            a="false"
+if a=="true":
+    print("it's a match")
+else:
+    print("it's gone")
